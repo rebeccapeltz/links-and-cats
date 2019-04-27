f72175c2396c00e34dfd4bdd6db874225b8fcd24
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-04-26 17:05:38 -0700

    add JS to handle show/hide public/private

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",event=>{
+  let option1 = document.querySelector("#option1")
+  let option2 = document.querySelector("#option2")
+  let option3 = document.querySelector("#option3")
+  let publicRow = document.querySelector(".public-row")
+  let privateRow = document.querySelector(".private-row")
+  option1.addEventListener("click",event=>{
+    event.stopPropagation()
+    publicRow.style.display = "flex"
+    privateRow.style.display = "flex"
+  })
+  option2.addEventListener("click",event=>{
+    event.stopPropagation()
+    publicRow.style.display = "flex"
+    privateRow.style.display = "none"
+  })
+  option3.addEventListener("click",event=>{
+    event.stopPropagation()
+    publicRow.style.display = "none"
+    privateRow.style.display = "flex"
+  })
+})
