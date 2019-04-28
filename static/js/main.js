5c4393bd7eaa3874b5a7be519e7086477460e552
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-04-28 13:48:03 -0700

    add show/hide toggle to update link

--- static/js/main.js
+++ static/js/main.js
@@ -1,67 +1,81 @@
 document.addEventListener("DOMContentLoaded", event => {
-  let option1 = document.querySelector("#option1")
-  let option2 = document.querySelector("#option2")
-  let option3 = document.querySelector("#option3")
-  let publicRow = document.querySelector(".public-row")
-  let privateRow = document.querySelector(".private-row")
-  if (option1 && option2 && option3) {
-    option1.addEventListener("click", event => {
-      event.stopPropagation()
-      publicRow.style.display = "flex"
-      privateRow.style.display = "flex"
-    })
-    option2.addEventListener("click", event => {
-      event.stopPropagation()
-      publicRow.style.display = "flex"
-      privateRow.style.display = "none"
-    })
-    option3.addEventListener("click", event => {
-      event.stopPropagation()
-      publicRow.style.display = "none"
-      privateRow.style.display = "flex"
-    })
+  if (window.location.href.indexOf("index") > -1) {
+    let option1 = document.querySelector("#option1")
+    let option2 = document.querySelector("#option2")
+    let option3 = document.querySelector("#option3")
+    let publicRow = document.querySelector(".public-row")
+    let privateRow = document.querySelector(".private-row")
+    if (option1 && option2 && option3) {
+      option1.addEventListener("click", event => {
+        event.stopPropagation()
+        publicRow.style.display = "flex"
+        privateRow.style.display = "flex"
+      })
+      option2.addEventListener("click", event => {
+        event.stopPropagation()
+        publicRow.style.display = "flex"
+        privateRow.style.display = "none"
+      })
+      option3.addEventListener("click", event => {
+        event.stopPropagation()
+        publicRow.style.display = "none"
+        privateRow.style.display = "flex"
+      })
+    }
   }
 
   //category manager
-  let catManBtns = document.querySelectorAll(".cat-man-btn")
-  for (let btn of catManBtns) {
-    btn.addEventListener('click', function (event) {
-      let i = $(this).data('idx');
-      let catOrigDescription = document.querySelector(`#cat-orig${i}`).innerHTML
-      let catNewDescription = document.querySelector(`#cat-new${i}`).value
-      // alert(catNewDescription.value)
-      let tempForm = document.createElement('form');
-      tempForm.setAttribute('action', '/manage_categories');
-      tempForm.setAttribute('method', 'post');
-      tempForm.setAttribute('hidden', 'true');
-      //add original value
-      let descOrigInput = document.createElement('input');
-      descOrigInput.setAttribute('type', 'text');
-      descOrigInput.setAttribute('name', 'desc-input-orig');
-      descOrigInput.setAttribute('value', catOrigDescription);
-      tempForm.appendChild(descOrigInput);
-      //add new value
-      let descNewInput = document.createElement('input');
-      descNewInput.setAttribute('type', 'text');
-      descNewInput.setAttribute('name', 'desc-input-new');
-      descNewInput.setAttribute('value', catNewDescription);
-      tempForm.appendChild(descNewInput);
+  if (window.location.href.indexOf("manage_categories") > -1) {
 
-      document.body.appendChild(tempForm);
-      tempForm.submit();
-    })
+    let catManBtns = document.querySelectorAll(".cat-man-btn")
+    for (let btn of catManBtns) {
+      btn.addEventListener('click', function (event) {
+        let i = $(this).data('idx');
+        let catOrigDescription = document.querySelector(`#cat-orig${i}`).innerHTML
+        let catNewDescription = document.querySelector(`#cat-new${i}`).value
+        // alert(catNewDescription.value)
+        let tempForm = document.createElement('form');
+        tempForm.setAttribute('action', '/manage_categories');
+        tempForm.setAttribute('method', 'post');
+        tempForm.setAttribute('hidden', 'true');
+        //add original value
+        let descOrigInput = document.createElement('input');
+        descOrigInput.setAttribute('type', 'text');
+        descOrigInput.setAttribute('name', 'desc-input-orig');
+        descOrigInput.setAttribute('value', catOrigDescription);
+        tempForm.appendChild(descOrigInput);
+        //add new value
+        let descNewInput = document.createElement('input');
+        descNewInput.setAttribute('type', 'text');
+        descNewInput.setAttribute('name', 'desc-input-new');
+        descNewInput.setAttribute('value', catNewDescription);
+        tempForm.appendChild(descNewInput);
+
+        document.body.appendChild(tempForm);
+        tempForm.submit();
+      })
+    }
 
     //click on fa-edit and show input and submit button
     // upd-cat{{loop.index0}}
+    function showHideUpdate(ev) {
+      // this ought to keep t-daddy from getting the click.
+      ev.stopPropagation();
+      // alert("event propagation halted.");
+      let idx = this.dataset.idx;
+      // get matching update controls
+      let editSpan = document.querySelector(`#upd-cat${idx}`)
+      // toggle show/hide
+      editSpan.classList.toggle("none")
+      editSpan.classList.toggle("show-inline")
+    }
     let editIcons = document.querySelectorAll(".fa-edit");
-    for (let icon of  editIcons){
-      icon.addEventListener("click",function(event){
-        let i = $(this).data('idx');
-        let editSpan = document.querySelector(`#upd-cat${i}`)
-        editSpan.style.display = "inline-block";
-      })
+    for (let icon of editIcons) {
+      icon.addEventListener("click", showHideUpdate, false)
     }
 
     //click on fa-trash-alt and get orignal value and delete it
   }
+
+
 })
